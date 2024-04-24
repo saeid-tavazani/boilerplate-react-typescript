f1495525b47fb579a3f153dbac83433b6af2f03d
saeid tavazani <saeid.ir82s@gmail.com>
2024-04-24 01:00:09 -0700

    fix function add product && product quantity

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,15 +1,54 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type CardProviderProps = {
   children: ReactNode;
 };
+type CartItem = {
+  id: number;
+  qty: number;
+};
+type CartContext = {
+  getItemQty: (id: number) => number;
+  addItem: (id: number) => void;
+  decreaseItem: (id: number) => void;
+  removeItem: (id: number) => void;
+};
 
-const CartContext = createContext({});
+const CartContext = createContext({} as CartContext);
 
 export const useCartContext = () => {
   return useContext(CartContext);
 };
 
 export const CardProvider = ({ children }: CardProviderProps) => {
-  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const getItemQty = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.qty || 0;
+  };
+  const addItem = (id: number) => {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, qty: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, qty: item.qty + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+  const decreaseItem = (id: number) => {};
+  const removeItem = (id: number) => {};
+
+  return (
+    <CartContext.Provider
+      value={{ getItemQty, addItem, decreaseItem, removeItem }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
