0ab5c6b4e19ee15ef50424c6f3a55b38596b3e2f
saeid tavazani <saeid.ir82s@gmail.com>
2024-04-24 01:20:17 -0700

    fix event

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from "react-bootstrap";
+import { useCartContext } from "../context/CartContext";
 
 type ProductProps = {
   id: number;
@@ -8,7 +9,8 @@ type ProductProps = {
 };
 
 const Product = ({ id, title, price, imgUrl }: ProductProps) => {
-  const qty = 0;
+  const { addItem, getItemQty, decreaseItem, removeItem } = useCartContext();
+  const qty = getItemQty(id);
   return (
     <Card className="h-100">
       <Card.Img
@@ -25,7 +27,9 @@ const Product = ({ id, title, price, imgUrl }: ProductProps) => {
         </Card.Title>
         <div className="mt-auto">
           {qty === 0 ? (
-            <Button className="w-100 btn-secondary">Add to Cart</Button>
+            <Button className="w-100 btn-secondary" onClick={() => addItem(id)}>
+              Add to Cart
+            </Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
@@ -35,11 +39,22 @@ const Product = ({ id, title, price, imgUrl }: ProductProps) => {
                 className="d-flex align-items-center justify-content-center"
                 style={{ gap: "0.5rem" }}
               >
-                <Button className="btn-secondary">+</Button>
+                <Button className="btn-secondary" onClick={() => addItem(id)}>
+                  +
+                </Button>
                 <span className="fs-5 m-3 text-light">{qty}</span>
-                <Button className="btn-secondary">-</Button>
+                <Button
+                  className="btn-secondary"
+                  onClick={() => decreaseItem(id)}
+                >
+                  -
+                </Button>
               </div>
-              <Button className="btn-ligth" size="sm">
+              <Button
+                onClick={() => removeItem(id)}
+                className="btn-dark"
+                size="sm"
+              >
                 Remove
               </Button>
             </div>
